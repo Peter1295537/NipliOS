6ecc62c5a9518bf30caa30356295978c3b1de9c5
Peter1295537 <datkoolkidpeter@gmail.com>
2017-10-04 17:11:43 -0400

    Simple shell in c++

---
+++ NipliOS.cpp
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <signal.h>
+#include <errno.h>
+#include <sys/types.h>
+
+void signal_handler(int no) {
+}
+
+void array_maker(char input[], char *array[], int in, int n) {
+		
+}
+
+int main() {
+	char command[50];
+	char *token;
+	
+	
+	signal(SIGINT, signal_handler);
+
+	char *array[50];
+	pid_t child;
+	
+	while(1) {	
+		printf("NipliOS>");
+		fgets(command, 50, stdin);
+		token=strtok(command, " ");
+
+		if (strncmp(token, "PROC",4)==0) {
+			printf("Process unfinished \n");
+		}
+		else if (strncmp(token, "LOAD",4)==0) {
+			printf("Loading \n");
+		}
+		else if (strncmp(token, "MEM",3)==0) {
+			printf("Total Memory Used: \n") ;
+		}
+		else if (strncmp(token, "RESET",5)==0) {
+			printf("System Reset \n");
+		}
+                else if (strncmp(token, "exit",4)==0) {
+                        raise(SIGKILL);
+                }
+
+	}
+	return 0;
+}
