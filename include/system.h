c7789a3dcd1d0fc0873551f059b06a55fdfa2245
rangerlatham <ghanklatham@gmail.com>
2017-11-28 19:41:22 -0500

    halfy commit

--- include/system.h
+++ include/system.h
@@ -1,11 +1,23 @@
 #include <vector>
+
+#include <semaphore.h>
+#include <pthread.h>
+#include <sys/types.h>
+#include <errno.h>
+#include <unistd.h>
+#include <stdlib.h>
+
 #include "../include/process_class.h"
 #include "../include/mem_mgmt.h"
+#include "../include/rr.h"
+#include "../include/queue.h"
 
 using namespace std;
 
 class system{
 	private:
+		sem_t semaphore;
+
 		vector<process*> WAIT;
 		vector<process*> NEW;
 		vector<process*> READY;
@@ -24,9 +36,24 @@ class system{
 		rr fore(50);
 
 		int start=0;
+		
+		struct cpuThreadParams params;
+		void *cpu_processing(void *);
 
 
 	public:
+		struct cpuThreadParams {
+			queue* one;
+			queue* two;
+			queue* three;
+
+			memory* main;
+
+			rr* first;
+			rr* second;
+
+			int* start;
+		}
 		int spawn(string);
 		int kill(process*,string);
 		int addReady(process*);
