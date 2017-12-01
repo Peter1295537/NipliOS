7690d9abaf03437c46fb092f0388331da9b12eb3
Peter1295537 <datkoolkidpeter@gmail.com>
2017-11-30 21:40:59 -0500

    Added man and gen command

--- data/procGen.py
+++ data/procGen.py
@@ -1,9 +1,10 @@
 import random
 
-num=input("Enter number of processes: ")
+num=input()
+
 int(num)
-for x in range (0,num):
-	process=open("process%d"%(x), "w")
+for x in range (1,num+1):
+	process=open("../data/process%d"%(x), "w")
 	process.write("%d\n"%(random.randint(100,1000)))
 	for y in range (0, random.randint(10, 25)):
 		dice=random.randint(0,6)
